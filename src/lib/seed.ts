345d9fbff42a8603e8fb207e625d4e341ae747df
PhDBui <congdoan6997@gmail.com>
2024-06-03 23:42:46 +0300

    fix: 🐛 fix seed

--- src/lib/seed.ts
+++ src/lib/seed.ts
@@ -1,13 +1,10 @@
 import { Redis } from "@upstash/redis";
 
-// const redis = new Redis({
-//   url: process.env.UPSTASH_REDIS_REST_URL!,
-//   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-// })
 const redis = new Redis({
-  url: "https://united-frog-51388.upstash.io",
-  token: "Aci8AAIncDE3ZTE0MzVmMWE2ZTI0MzMyYmYwZDM4OWE3OTA4NTI0N3AxNTEzODg",
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
+
 const countryList = [
   "Afghanistan",
   "Albania",
